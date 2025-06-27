2c327af239c9e9fc2b64c0c9b6bd73dbd7c8c1bf
zhangyu <1838792979@qq.com>
2025-06-27 10:32:01 +0800

    '完善'

--- bin/bin.js
+++ bin/bin.js
@@ -3,6 +3,75 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 const inquirer_1 = require("inquirer");
+const ignoreTemplates = new Map([
+    [".gitignore", `# Dependency directories
+node_modules/
+dist/
+build/
+
+# Environment files
+.env
+.env.local
+.env.development
+.env.test
+.env.production
+
+# Editor directories and files
+.idea
+.vscode
+*.suo
+*.ntvs*
+*.njsproj
+*.sln
+*.sw?`],
+    [".npmignore", `# Logs
+logs
+*.log
+npm-debug.log*
+
+# Dependency directories
+node_modules/
+
+# Environment files
+.env
+.env.local
+
+# Build output
+dist/
+build/
+out/
+
+# Coverage
+coverage/
+.nyc_output
+
+# Editor directories and files
+.idea
+.vscode
+*.suo
+*.ntvs*
+*.njsproj
+*.sln
+*.sw?`],
+    [".eslintignore", `node_modules/
+dist/
+build/
+coverage/
+.env
+.env.local`],
+    [".prettierignore", `node_modules/
+dist/
+build/
+coverage/
+.env
+.env.local`],
+    [".babelignore", `node_modules/
+dist/
+build/
+coverage/
+.env
+.env.local`]
+]);
 const questions = [
     {
         type: "list",
@@ -18,5 +87,5 @@ const questions = [
     }
 ];
 inquirer_1.default.prompt(questions).then((ans) => {
-    fs.writeFileSync(`${process.cwd()}/${ans.option}`, '');
+    fs.writeFileSync(`${process.cwd()}/${ans.option}`, ignoreTemplates.get(ans.option) || '', 'utf-8');
 });
--- bin/bin.ts
+++ bin/bin.ts
@@ -1,8 +1,81 @@
 #!/usr/bin/env node
 
 import * as fs from "fs"
-import * as path from "path"
 import inquirer, {type Question } from "inquirer"
+
+const ignoreTemplates = new Map<string, string>([
+    [".gitignore", `# Dependency directories
+node_modules/
+dist/
+build/
+
+# Environment files
+.env
+.env.local
+.env.development
+.env.test
+.env.production
+
+# Editor directories and files
+.idea
+.vscode
+*.suo
+*.ntvs*
+*.njsproj
+*.sln
+*.sw?`],
+
+    [".npmignore", `# Logs
+logs
+*.log
+npm-debug.log*
+
+# Dependency directories
+node_modules/
+
+# Environment files
+.env
+.env.local
+
+# Build output
+dist/
+build/
+out/
+
+# Coverage
+coverage/
+.nyc_output
+
+# Editor directories and files
+.idea
+.vscode
+*.suo
+*.ntvs*
+*.njsproj
+*.sln
+*.sw?`],
+
+    [".eslintignore", `node_modules/
+dist/
+build/
+coverage/
+.env
+.env.local`],
+
+    [".prettierignore", `node_modules/
+dist/
+build/
+coverage/
+.env
+.env.local`],
+
+    [".babelignore", `node_modules/
+dist/
+build/
+coverage/
+.env
+.env.local`]
+]);
 
 const questions: Question[] = [
     {
@@ -20,5 +93,5 @@ const questions: Question[] = [
 ]
 
 inquirer.prompt(questions as any).then((ans) => {
-    fs.writeFileSync(`${process.cwd()}/${ans.option}`, '')
+    fs.writeFileSync(`${process.cwd()}/${ans.option}`, ignoreTemplates.get(ans.option) || '', 'utf-8')
 })
